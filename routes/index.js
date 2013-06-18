cd11a77cdd187bcdbad7f93737b00f1578462fa4
Jesse Patching <jesse@patching.ca>
2013-06-18 08:50:56 -0600

    Simplified log

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@
  * Recieve a POST from Amazon SNS
  */
 exports.index = function(req, res){
-    console.log("Got Amazon SNS Message: " + req.body);
+    console.log(req.body);
+    res.send('ok');
 };
